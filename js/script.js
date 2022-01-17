2c56342ef476eb7a14f729dec71142accddf8c5c
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-01-17 15:54:42 +0100

    using splice method to remove an item from the list (script 33)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+//Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
+//- text, una stringa che indica il testo del todo
+//- done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+
+// MILESTONE 1
+// Stampare all’interno di una lista, un item per ogni todo. 
+//Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+
+// MILESTONE 2
+// Visualizzare a fianco ad ogni item ha una “x”: 
+//cliccando su di essa, il todo viene rimosso dalla lista.
+
 new Vue({
     el:"#app",
     data: {
@@ -15,5 +27,10 @@ new Vue({
                 done: false,
             },
         ],
+    },
+    methods: {
+        removeItemList: function(index) {
+            this.todo.splice(index, 1); //significa:"voglio cancellare UN item partendo dall'INDEX iniziale"
+        },
     },
 });
