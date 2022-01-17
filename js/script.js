3d018566d08975642b6f448e60610dda31621f08
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-01-17 15:03:13 +0100

    array of objects

---
+++ js/script.js
@@ -0,0 +1,21 @@
+new Vue({
+    el:"#app",
+    data: {
+        todo: [
+            {
+                text: "Fare i compiti",
+                done: false,
+            },
+            {
+                text: "Fare la spesa",
+                done: true,
+            },
+            {
+                text: "Fare il bucato",
+                done: false,
+            }
+         ],
+
+    }
+
+});
